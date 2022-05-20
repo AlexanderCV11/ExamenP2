b011531ea5a73c66a614af8238a4c5483b224dad
alexa <alexander_corral_valdez@outlook.com>
2022-05-20 11:16:08 -0500

    Cambio3

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, Image} from 'react-native';
 import styles from './styles';
+import Logo from './assets/Burris-logo.gif';
+
 
 function HomeScreen({navigation}) {
     return (
         <View style={styles.container}>
+            <Image source={{uri: Logo}}/>
             <Text style={styles.text}>Home</Text>
             <Button 
-                title="Ir a Detalle"
+                title="Ir a la tienda"
                 onPress={() => navigation.navigate('Tasks', {
                     id: 1, 
                     description: 'Aquí va la descripción',

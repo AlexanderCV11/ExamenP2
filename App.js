bfb264644c227c5d9d2d1f88c1220435041759c5
alexa <alexander_corral_valdez@outlook.com>
2022-05-20 10:21:11 -0500

    Cambio

---
+++ App.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import HomeScreen from './HomeScreen';
+import DetailScreen from './DetailScreen';
+import MenuScreen from './MenuScreen';
+
+
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName='Home'>
+        <Drawer.Screen name="Home" component={HomeScreen}/>
+        <Drawer.Screen name="Tasks" component={MenuScreen} options={{title: "Tienda"}} />
+        <Drawer.Screen name="Details" component={DetailScreen} options={{title: "Producto"}} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
